d37a716ef33c09b4d0d3d53a8d1c4a955dd600a5
Andy <huylv.dev@gmail.com>
2024-05-06 17:46:26 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "",
+//   build: {
+//     chunkSizeWarningLimit: 3000,
+//   },
+// });
+
+import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vercel()],
   base: "",
   build: {
     chunkSizeWarningLimit: 3000,
